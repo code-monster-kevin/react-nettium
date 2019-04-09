1c14d32aa85725895a17916a298d78b9d08060ac
Kevin Wu Tse Ek <kevin.wu@live.com.my>
2019-04-09 17:32:06 +0800

    reset to starting point

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './component/basic/AppIntl';
-// import App from './App';
+// import App from './component/basic/App2';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
